5845dff2479568af1a0e76a6f952850bda5f85e5
antonfedenko7725 <anton.fedenko7725@gmail.com>
2026-04-27 14:20:41 +0300

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -20,6 +20,15 @@ VOICES = [
     {"id": "7", "file_id": "AwACAgIAAxkBAAMRaespJ6_pj9OrgEe3fkbUJQcssMAAAgUHAAKJSWFKx0HP2AdoI1o7BA", "title": "Антон прийшов чи нє"},
     {"id": "8", "file_id": "AwACAgIAAxkBAAMTaespMafhRZ0bhnXwGt13t9XjJdQAAhoHAAKJSWFKRplM3SRq5f47BA", "title": "Я п'яний в щі"},
     {"id": "9", "file_id": "AwACAgIAAxkBAAMVaespQzXQ8vWM9bbx_P2m106kVyIAAmMGAAL9l7hLz9BsJt4s6MY7BA", "title": "Шева тут пізда"},
+    {"id": "10", "file_id": "AwACAgIAAxkBAAMXae9B4Lc80CBSGyWCFFY4QfluUpwAApWLAAKM3FBJWYAhvYGkkjY7BA", "title": "мєн ю кен"},
+    {"id": "11", "file_id": "AwACAgIAAxkBAAMbae9E00TnIfge5yKlmAlztvMsoAQAAq6LAAKM3FBJIgS4yDjaf807BA", "title": "ти сука дебіл"},
+    {"id": "12", "file_id": "AwACAgIAAxkBAAMdae9FIz1t5kVM8adPBVKHDRfJJoAAAoADAAK8mylLF7APgtQAAYgAATsE", "title": "Я блять питаю"},
+    {"id": "13", "file_id": "AwACAgIAAxkBAAMgae9FjnWou4GL_W9Oa_5R1hGaUF8AAnQEAALBv3hI5Ynw8fcgwcU7BA", "title": "Го бухать нахуй"},
+    {"id": "14", "file_id": "AwACAgIAAxkBAAMiae9Fug39a6jQiVqA8bbN2PDsHYoAAtgDAAJPHNhIMz1-S0pO6cA7BA", "title": "Забув як ти водку жрав"},
+    {"id": "15", "file_id": "AwACAgIAAxkBAAMkae9F3r0QDMjbMdwK_rxczIzstDEAAqgDAALsjZhJpPUDYqXwlFU7BA", "title": "Вадім підар"},
+    {"id": "16", "file_id": "AwACAgIAAxkBAAMmae9F_lJY9PaG-dVkHBkoL_WVfP4AAvICAALlJ_hLZeV9bxEtzoI7BA", "title": "Пососи грєбєнь"},
+    {"id": "10", "file_id": "AwACAgIAAxkBAAMoae9GZMoehSj0Z6F3qfAMfPpDSDoAAjMCAALaPAhJccr1IjGxWaI7BA", "title": "Пачка сігарєт"},
+    
 ]
 
 # --- НОВИЙ БЛОК: СЛУХАЄМО НОВІ ГОЛОСОВІ ---
